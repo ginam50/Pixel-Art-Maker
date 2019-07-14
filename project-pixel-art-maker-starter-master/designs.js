0066aea66f73f1a1ba3334bbafb3300031c6b750
ginam50 <48866158+ginam50@users.noreply.github.com>
2019-07-14 07:53:43 -0400

    Update designs.js
    Updated grid code.  Here are instructions from mentor for the coloring code: Hi Regina, To complete the color functionality you need to do 2 things: You want to add the click eventlistener for every created cell of the grid, so we should add the event listener inside one of the for loops, as it creates the user input number cells for every column.  Then we want to use the user chosen color from the colors options, which is found in this html element: <input type="color" id="colorPicker">   So we want to link this element from the html to all grid cells, which we named its variable as 'column' and then assign the value of the input to that cell.
    
    So we need to change "color.value" and instead use the id of the color input html element's value.
    Profile image
    Ibrahim E
    about 10 hours ago

--- project-pixel-art-maker-starter-master/designs.js
+++ project-pixel-art-maker-starter-master/designs.js
@@ -1,44 +1,32 @@
 // Select color input and store
-var color = document.getElementById('colorPicker').value;
-
+var color = document.getElementById("colorPicker");
 // Select size input and store
-var sizePicker = document.getElementById('sizePicker');
+var sizePicker = document.getElementById("sizePicker");
+// Define variables and store values for DOM elements
+var canvas = document.getElementById("pixelCanvas");
 
-// Define and store variables for other DOM elements
-var canvas = document.getElementById('pixelCanvas');
-var height = document.getElementById('inputHeight').value;
-var width = document.getElementById('inputWidth').value;
-
-// When size is submitted by the user, call makeGrid()
-function makeGrid() {
-
-    var canvas = document.getElementById('pixelCanvas');
-    
-    var height = document.getElementById('inputHeight').value;
-    
-    var width = document.getElementById('inputWidth').value;
-    //loop over each row    
-    for (var m = 0; m < height; m++) {
-
-        var row = document.createElement('tr');
-        
-        canvas.appendChild(row);
-        
-        for (var n = 0; n < width; n++) {
-
-            var column = document.createElement('td');
-            
-            canvas.appendChild(column);
-
-            column.addEventListener('click', function (event) {
-            // color when a cell is clicked
-            event.target.style.backgroundColor = color.value; 
-        });
-    }
-
-sizePicker.addEventListener('submit', function (event) {
-    // prevent created grid from being erased
-    event.preventDefault(); 
-    canvas.innerHTML = null;
+sizePicker.addEventListener("submit", function(event) {
+    event.preventDefault();
     makeGrid();
 });
+
+// When size is submitted by the user, call makeGrid()
+
+function makeGrid() {
+var height = document.getElementById("inputHeight").value;
+var width = document.getElementById("inputWidth").value;
+canvas.innerHTML = null;
+
+canvas.addEventListener('click', function(event) {
+// color when a cell is clicked
+event.target.style.backgroundColor = color.value;
+
+//loop over each row    
+for (var m = 0; m < height; m++) {
+    var row = canvas.insertRow(m);
+    for (var n = 0; n < width; n++) {
+        var column = row.insertCell(n);             
+        }
+    }
+});
+}
